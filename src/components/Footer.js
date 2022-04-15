036d4b2fc797e92308e5a638398c6ad84dfdfc82
Sajal Ganjewala <sg748@snu.edu.in>
2022-04-15 09:56:29 +0530

    OKay

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <div>
+            <h3>
+                This is the Footer
+            </h3>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+    return (
+        <div>
+            <div>
+                <h2>
+                    This is the Header
+
+                </h2>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header;
